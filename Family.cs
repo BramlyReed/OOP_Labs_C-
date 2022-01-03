be71ffd4fc5a387a0daf14cf4b597439030bacde
BramlyReed <ya.andreev99@ya.ru>
2022-01-03 22:30:15 +0500

    Смена битового И на логическое

---
+++ Family.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_lab1
+{
+
+	enum Sex { male, female };
+
+	class Person
+	{
+		private string name;
+		private Sex sex;
+		private Person dad;
+		private Person mom;
+		private Person partner;
+		private List<Person> child;
+
+		public Person(string name, Sex sex)
+		{
+			this.name = name;
+			this.sex = sex;
+		}
+
+		public void SetMarriedValue(Person m_partner)
+		{
+			partner = m_partner;
+			m_partner.partner = this;
+		}
+
+		private Boolean CheckPersonNotNull(Person p)
+		{
+			if (p != null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		private Boolean CheckThatArrayExist(List<Person> list)
+		{
+			if (list != null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		private void PrintNothing(String message)
+		{
+			Console.WriteLine($"По данному запросу - {message} - ничего не было найдено.");
+		}
+
+		private void SetChild(Person child)
+		{
+			if (this.child != null)
+			{
+				this.child.Add(child);
+			}
+			else
+			{
+				List<Person> children = new List<Person>();
+				children.Add(child);
+				this.child = children;
+			}
+		}
+
+		public void AddChild(Person child)
+		{
+			SetChild(child);
+			if (CheckPersonNotNull(partner))
+			{
+				partner.SetChild(child);
+			}
+			switch (sex)
+			{
+				case Sex.female:
+					child.mom = this;
+					child.dad = partner;
+					break;
+				case Sex.male:
+					child.dad = this;
+					child.mom = partner;
+					break;
+			}
+		}
+
+		public void PrintNamesOfParents()
+		{
+			if (!(CheckPersonNotNull(dad)) && !(CheckPersonNotNull(mom)))
+			{
+				PrintNothing($"{name} PrintNamesOfParents");
+			}
+			else
+			{
+				if (CheckPersonNotNull(dad)) { Console.WriteLine($"У {name} папа: {dad.name}"); }
+				if (CheckPersonNotNull(mom)) { Console.WriteLine($"У {name} мама: {mom.name}"); }
+			}
+		}
+
+		private List<Person> GetSiblings(Person person, Sex sex)
+		{
+			Person parent = CheckAndSetParent(person);
+			if (!(CheckPersonNotNull(parent)))
+			{
+				return null;
+			}
+			List<Person> list = new List<Person>();
+			foreach (Person p in parent.child)
+			{
+				if (p != person)
+				{
+					if (p.sex == sex)
+					{
+						list.Add(p);
+					}
+					else
+					{
+						if (CheckPersonNotNull(p.partner))
+						{
+							list.Add(p.partner);
+						}
+					}
+				}
+			}
+			return list;
+		}
+
+		private List<Person> GetPeople(Sex sex)
+		{
+			if (!(CheckPersonNotNull(dad)) && !(CheckPersonNotNull(mom))){
+				return null;
+			}
+			List<Person> list = new List<Person>();
+			List<Person> sublist = new List<Person>();
+			List<Person> sublist1 = new List<Person>();
+			if (CheckPersonNotNull(dad))
+			{
+				sublist = (GetSiblings(dad, sex));
+				if (CheckThatArrayExist(sublist)) { list.AddRange(sublist); }
+			}
+			if (CheckPersonNotNull(mom))
+			{
+				sublist1 = (GetSiblings(mom, sex));
+				if (CheckThatArrayExist(sublist1)) { list.AddRange(sublist1); }
+			}
+			return list;
+		}
+
+		public void PrintUncles() {
+			List<Person> list = GetPeople(Sex.male);
+			if (CheckThatArrayExist(list)) {
+				foreach (Person uncles in list) {
+					Console.WriteLine($"У {name} дядя: {uncles.name}");
+				}
+			}
+			else
+			{
+				PrintNothing($"{name} PrintUncles");
+			}
+		}
+
+		public void PrintAunties()
+		{
+			List<Person> list = GetPeople(Sex.female);
+			if (CheckThatArrayExist(list))
+			{
+				foreach (Person aunties in list)
+				{
+					Console.WriteLine($"У {name} тетя: {aunties.name}");
+				}
+			}
+			else
+			{
+				PrintNothing($"{name} PrintAunties");
+			}
+		}
+
+		private Person CheckAndSetParent(Person p)
+		{
+			Person person = null;
+			if (CheckPersonNotNull(p.dad)) { person = p.dad;}
+			else if (CheckPersonNotNull(p.mom)) { person = p.mom; }
+			return person;
+		}
+
+		private List<Person> GetCousinsFrom(Person person)
+		{
+			List<Person> cousins_list = new List<Person>();
+			Person parent = CheckAndSetParent(person);
+			if (CheckPersonNotNull(parent))
+			{
+				foreach (Person human in parent.child)
+				{
+					if (CheckThatArrayExist(human.child) && (human != dad && human != mom))
+					{
+						cousins_list.AddRange(human.child);
+					}
+				}
+			}
+			else { return null; }
+			return cousins_list;
+		}
+
+		private List<Person> GetAllCousins()
+		{
+			List<Person> cousin_list = new List<Person>();
+			List<Person> momLine = new List<Person>();
+			List<Person> dadLine = new List<Person>();
+
+			if (!(CheckPersonNotNull(mom)) && !(CheckPersonNotNull(dad))){
+				return null;
+			}
+			if (CheckPersonNotNull(mom)) {
+				momLine = GetCousinsFrom(mom);
+				if (CheckThatArrayExist(momLine))
+				{
+					cousin_list.AddRange(momLine);
+				}
+			}
+			if (CheckPersonNotNull(dad)) {
+				dadLine = GetCousinsFrom(dad);
+				if (CheckThatArrayExist(dadLine))
+				{
+					cousin_list.AddRange(dadLine);
+				}
+			}
+			return cousin_list;
+		}
+
+		public void PrintCousins()
+		{
+			List<Person> list = GetAllCousins();
+			if (CheckThatArrayExist(list))
+			{
+				foreach (Person cousin in list)
+				{
+					Console.WriteLine($"У {name} cousin: {cousin.name}");
+				}
+			}
+			else
+			{
+				PrintNothing($"{name} PrintCousins");
+			}
+		}
+
+		private List<Person> GetInLaws()
+		{
+			List<Person> laws_list = new List<Person>();
+			if (CheckPersonNotNull(partner))
+			{
+				if (!(CheckPersonNotNull(mom)) && (!CheckPersonNotNull(dad))) {
+					return null;
+				}
+				if (CheckPersonNotNull(partner.dad)) { laws_list.Add(partner.dad); }
+				if (CheckPersonNotNull(partner.mom)) { laws_list.Add(partner.mom); }
+			}
+			else
+			{
+				return null;
+			}
+			return laws_list;
+		}
+
+		public void PrintInLaws()
+		{
+			List<Person> list = GetInLaws();
+			if (CheckThatArrayExist(list))
+			{
+				foreach (Person laws in list)
+				{
+					Console.WriteLine($"У {name} In-laws: {laws.name}");
+				}
+			}
+			else
+			{
+				PrintNothing($"{name} PrintInLaws");
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using OOP_lab1;
+using System;
+using System.Collections.Generic;
+
+namespace HomeWorkOOP1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person me = new Person("Стас", Sex.male);
+			Person father = new Person("Анатолий", Sex.male);
+			Person mother = new Person("Юлия", Sex.female);
+			father.SetMarriedValue(mother);
+			father.AddChild(me);
+
+			Person fatherByFather = new Person("Валерий", Sex.male);
+			Person motherByFather = new Person("Наталия Глебовна", Sex.female);
+			Person fatherBrother = new Person("Иван", Sex.male);
+			Person fatherBrotherWife = new Person("Наталия", Sex.female);
+			Person fatherBrotherSon = new Person("Макар", Sex.male);
+
+			fatherByFather.SetMarriedValue(motherByFather);
+			fatherByFather.AddChild(father);
+			fatherByFather.AddChild(fatherBrother);
+
+			fatherBrother.SetMarriedValue(fatherBrotherWife);
+			fatherBrother.AddChild(fatherBrotherSon);
+
+
+			Person fatherByMother = new Person("Виктор", Sex.male);
+			Person motherByMother = new Person("Наталия Дмитриевна", Sex.female);
+			Person motherBrother = new Person("Константин", Sex.male);
+			Person motherBrotherWife = new Person("Юлия Константиновна", Sex.female);
+			Person motherBrotherSon1 = new Person("Илья", Sex.male);
+			Person motherBrotherSon2 = new Person("Андрей", Sex.male);
+			Person motherBrotherDaughter = new Person("Анна", Sex.female);
+
+			fatherByMother.SetMarriedValue(motherByMother);
+			fatherByMother.AddChild(mother);
+			fatherByMother.AddChild(motherBrother);
+
+			motherBrother.SetMarriedValue(motherBrotherWife);
+			motherBrother.AddChild(motherBrotherSon1);
+			motherBrother.AddChild(motherBrotherSon2);
+			motherBrother.AddChild(motherBrotherDaughter);
+
+			motherBrotherSon1.PrintNamesOfParents();
+			motherBrotherSon1.PrintUncles();
+			motherBrotherSon1.PrintAunties();
+			motherBrotherSon1.PrintCousins();
+			motherBrotherSon1.PrintInLaws();
+
+		}
+	}
+}
